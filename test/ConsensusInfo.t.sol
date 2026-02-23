ec01fa06c98974c634b35915a6b6fcb08b4a53a1
keyleu <kleuyckx@gmail.com>
2026-02-23 09:37:26 +0000

    move constants to dedicated file

--- test/ConsensusInfo.t.sol
+++ test/ConsensusInfo.t.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.13 <0.9.0;
 
 import {Test} from "forge-std/Test.sol";
 import {ConsensusInfo} from "../src/ConsensusInfo.sol";
+import {CONSENSUS_INFO_ADDRESS} from "../src/Constants.sol";
 
 /// @title ConsensusInfoTest
 /// @author NASD Inc.
@@ -10,12 +11,12 @@ import {ConsensusInfo} from "../src/ConsensusInfo.sol";
 contract ConsensusInfoTest is Test {
     function setUp() public {
         vm.etch(
-            ConsensusInfo.CONSENSUS_INFO_ADDRESS,
+            CONSENSUS_INFO_ADDRESS,
             hex"3373fffffffffffffffffffffffffffffffffffffffe14603957366020146024575f5ffd5b5f35806002116031575f5ffd5b545f5260205ff35b5f355f5560203560015500"
         );
 
         vm.prank(0xffffFFFfFFffffffffffffffFfFFFfffFFFfFFfE);
-        (bool success,) = ConsensusInfo.CONSENSUS_INFO_ADDRESS.call(abi.encode(uint256(1773826860000), uint64(0)));
+        (bool success,) = CONSENSUS_INFO_ADDRESS.call(abi.encode(uint256(1773826860000), uint64(0)));
         require(success);
     }
 
