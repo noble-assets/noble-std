fd24e715c510b0febf2aea357f704dd4ce33d3d7
keyleu <kleuyckx@gmail.com>
2026-02-23 11:32:49 +0000

    use system address in test

--- test/ConsensusInfo.t.sol
+++ test/ConsensusInfo.t.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.13 <0.9.0;
 
 import {Test} from "forge-std/Test.sol";
 import {ConsensusInfo} from "../src/ConsensusInfo.sol";
-import {CONSENSUS_INFO_ADDRESS} from "../src/Constants.sol";
+import {CONSENSUS_INFO_ADDRESS, SYSTEM_ADDRESS} from "../src/Constants.sol";
 
 /// @title ConsensusInfoTest
 /// @author NASD Inc.
@@ -15,7 +15,7 @@ contract ConsensusInfoTest is Test {
             hex"3373fffffffffffffffffffffffffffffffffffffffe14603957366020146024575f5ffd5b5f35806002116031575f5ffd5b545f5260205ff35b5f355f5560203560015500"
         );
 
-        vm.prank(0xffffFFFfFFffffffffffffffFfFFFfffFFFfFFfE);
+        vm.prank(SYSTEM_ADDRESS);
         (bool success,) = CONSENSUS_INFO_ADDRESS.call(abi.encode(uint256(1773826860000), uint64(0)));
         require(success);
     }
