d3770ac9eacd1e9ce4aa3cf12356f32e4f8b3176
keyleu <keyne@noble.xyz>
2026-02-25 09:36:59 +0000

    edit system address comment

--- src/Constants.sol
+++ src/Constants.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity >=0.8.13 <0.9.0;
 
-/// @dev The system address used internally by the Noble EVM to execute gated actions.
+/// @dev The address of the system account on Noble.
 address constant SYSTEM_ADDRESS = 0xffffFFFfFFffffffffffffffFfFFFfffFFFfFFfE;
 
 /// @dev The address of the ConsensusInfo system contract on Noble.
